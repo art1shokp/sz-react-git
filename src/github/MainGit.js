5c1dc862cf432e4baa0780bef558ac9fb06e26ea
art1shokp <art1shokp@yandex.ru>
2020-10-06 15:22:31 +0600

    feat: add MainGitHeader, MainIntro

---
+++ src/github/MainGit.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainGitHeader from './MainGitHeader';
+import MainIntro from './MianIntro';
+
+
+const MainGit = () => (
+    <main className='main'>
+        <MainGitHeader/>
+        <MainIntro/>
+    </main>
+)
+
+
+export default MainGit;
---
+++ src/github/MainGitHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+
+const MainGitHeader = () => (
+    <div className='main__header'>
+        Main header
+    </div>
+)
+
+export default MainGitHeader;
